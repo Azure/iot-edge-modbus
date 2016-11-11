109edf94f34977361b484dfe5dd97f944c9b3002
Damon Barry <damonb@microsoft.com>
2016-11-11 13:43:36 -0800

    tabs to spaces

--- modules/modbus_read/inc/modbus_read_common.h
+++ modules/modbus_read/inc/modbus_read_common.h
@@ -34,31 +34,31 @@
 
 typedef struct MODBUS_READ_OPERATION_TAG
 {
-	struct MODBUS_READ_OPERATION_TAG * p_next;
-	unsigned char unit_id;
-	unsigned char function_code;
-	unsigned short address;
-	unsigned short length;
-	unsigned char read_request[256];
-	int read_request_len;
+    struct MODBUS_READ_OPERATION_TAG * p_next;
+    unsigned char unit_id;
+    unsigned char function_code;
+    unsigned short address;
+    unsigned short length;
+    unsigned char read_request[256];
+    int read_request_len;
 }MODBUS_READ_OPERATION;
 
 typedef struct MODBUS_READ_CONFIG_TAG
 {
-	struct MODBUS_READ_CONFIG_TAG * p_next;
-	MODBUS_READ_OPERATION * p_operation;
-	size_t read_interval;
-	char server_str[16];
-	char mac_address[18];
-	char device_type[64];
-	SOCKET_TYPE socks;
-	FILE_TYPE files;
-	size_t time_check;
-	int(*encode_read_cb)(void*, void*, void*);
-	int(*encode_write_cb)(void*, void*, unsigned char, unsigned short, unsigned short);
-	int(*decode_response_cb)(void*, void*, void*);
-	int(*send_request_cb)(struct MODBUS_READ_CONFIG_TAG *, unsigned char*, int, unsigned char*);
-	void(*close_server_cb)(struct MODBUS_READ_CONFIG_TAG *);
+    struct MODBUS_READ_CONFIG_TAG * p_next;
+    MODBUS_READ_OPERATION * p_operation;
+    size_t read_interval;
+    char server_str[16];
+    char mac_address[18];
+    char device_type[64];
+    SOCKET_TYPE socks;
+    FILE_TYPE files;
+    size_t time_check;
+    int(*encode_read_cb)(void*, void*, void*);
+    int(*encode_write_cb)(void*, void*, unsigned char, unsigned short, unsigned short);
+    int(*decode_response_cb)(void*, void*, void*);
+    int(*send_request_cb)(struct MODBUS_READ_CONFIG_TAG *, unsigned char*, int, unsigned char*);
+    void(*close_server_cb)(struct MODBUS_READ_CONFIG_TAG *);
 }MODBUS_READ_CONFIG; /*this needs to be passed to the Module_Create function*/
 
 #endif /*MODBUS_READ_COMMON_H*/
