1984ae9cf833c9b34939a25a7f4aad967484e647
Daniel Adolfsson <daniel.adolfsson@tetrapak.com>
2019-08-30 15:01:07 +0200

    Added the possibility to add additional properties to each slave config. If defined, these properties will be included in each output message.

--- IoTEdgeModbus/Modules/AzureIoTEdgeModbus/Slave/ModbusOutContent.cs
+++ IoTEdgeModbus/Modules/AzureIoTEdgeModbus/Slave/ModbusOutContent.cs
@@ -1,10 +1,16 @@
-﻿namespace AzureIoTEdgeModbus.Slave
+﻿using Newtonsoft.Json;
+
+namespace AzureIoTEdgeModbus.Slave
 {
     using System.Collections.Generic;
 
     public class ModbusOutContent
     {
         public string HwId { get; set; }
+
         public List<ModbusOutData> Data { get; set; }
+
+        [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Dictionary<string, string> AdditionalProperties { get; set; }
     }
 }
